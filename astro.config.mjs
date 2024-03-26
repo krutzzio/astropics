525af11122277d70184dcb6d27827b2f2f69c181
Krutzzio <gerard.costa.curr@gmail.com>
2024-03-26 17:56:31 +0100

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  adapter: netlify()
 });
